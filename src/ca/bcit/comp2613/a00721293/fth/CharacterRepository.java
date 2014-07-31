f58c0a7440bf815bfe1cb809eb1c9b19421fa148
a00721293 <andytung215@hotmail.com>
2014-07-30 18:54:38 -0700

    final exam

---
+++ src/ca/bcit/comp2613/a00721293/fth/CharacterRepository.java
@@ -0,0 +1,10 @@
+package ca.bcit.comp2613.a00721293.fth;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface CharacterRepository extends CrudRepository<Character, String>
+{
+
+}
